d046f0968487e12322275a4119a6b76757472b2d
Bikatr7 <Bikatr7@proton.me>
2024-06-21 09:05:54 -0600

    customized home page a bit more. coloring still out of date

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,12 +3,17 @@ import {
     AbsoluteCenter,
     Box,
     Button,
+    Image,
     Divider,
     Flex,
     Heading,
     Stack,
     Text
 } from "@chakra-ui/react";
+
+import { IconBrandGithub } from '@tabler/icons-react';
+
+import face from '../assets/images/kadenbilyeu.png';
 
 function HomePage() {
     useEffect(() => {
@@ -17,29 +22,45 @@ function HomePage() {
 
     return (
         <>
-            <Flex
-                direction={{ base: 'column', md: 'row' }}
-                align="center"
-                justify="center"
-                minH="100vh"
-                bg="black"
-                color="silver"
-                textAlign="center"
-                px={4}
-            >
-                <Stack spacing={6} maxW="xl" align="center">
+            <Preface />
+            <Box position="relative" padding="10">
+                <Divider />
+                <AbsoluteCenter bg="gray.800" px="4" id="introduction">
+                    Introduction
+                </AbsoluteCenter>
+            </Box>
+        </>
+    );
+}
+
+function Preface() {
+    return (
+        <Stack direction={{ base: 'column', md: 'row' }}>
+            <Flex p={8} flex={1} align="center">
+                <Stack spacing={6} w="full" maxW="xl">
                     <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
-                        Welcome to Kakusui
+                        <Text as="span" position="relative">
+                            Kaden Bilyeu
+                        </Text>
+                        <br />
                     </Heading>
-                    <Text fontSize={{ base: 'md', lg: 'lg' }} color="gray.300">
-                        This is a dummy page.
+                    <Text fontSize={{ base: 'md', lg: 'lg' }} color="gray.500">
+                        Computer science student at UCCS focusing on incorporating AI, LLMs, data science, and NER/NLP into software for real-world applications.
                     </Text>
-                    <Button colorScheme="yellow" bg="yellow.500" color="black" _hover={{ bg: "yellow.400" }}>
-                        Click me
-                    </Button>
+                    <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
+                        <Button rounded="full" bg="orange.400" color="white" as="a" href="#introduction" _hover={{ bg: 'orange.500' }}>
+                            More about me
+                        </Button>
+                        <Button as="a" href="https://github.com/Bikatr7" leftIcon={<IconBrandGithub />} rounded="full">
+                            My Github
+                        </Button>
+                    </Stack>
                 </Stack>
             </Flex>
-        </>
+            <Flex flex={1}>
+                <Image boxSize={400} alt="Kaden Bilyeu's Profile Picture" objectFit="cover" src={face} />
+            </Flex>
+        </Stack>
     );
 }
 
