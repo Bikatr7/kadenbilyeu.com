f93fc45f2e7a6cc1c29a27957f77dc05b5d76059
Bikatr7 <Bikatr7@proton.me>
2024-06-22 14:24:54 -0600

    added copyright headers

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,5 @@
+// Copyright 2024 Kaden Bilyeu (Bikatr7) (https://github.com/Bikatr7) (https://github.com/Bikatr7/kadenbilyeu.com) (https://kadenbilyeu.com)
+// Use of this source code is governed by a GNU General Public License v3.0
+// license that can be found in the LICENSE file.
+
 /// <reference types="vite/client" />
