7940a4c0d69f7510158fbc54f7894c750bf1f592
Bikatr7 <Bikatr7@proton.me>
2024-07-23 16:51:59 -0600

    separated site page into two separate parts and implemented lazy loading

--- frontend/src/components/NamedDivider.tsx
+++ frontend/src/components/NamedDivider.tsx
@@ -2,22 +2,36 @@
 // Use of this source code is governed by an GNU Affero General Public License v3.0
 // license that can be found in the LICENSE file
 
-// chakra-ui imports
-import {
-    AbsoluteCenter,
-    Box,
-    Divider
-} from "@chakra-ui/react";
+// Copyright 2024 Kaden Bilyeu (Bikatr7) (https://github.com/Bikatr7) (https://github.com/Bikatr7/kadenbilyeu.com) (https://kadenbilyeu.com)
+// Use of this source code is governed by an GNU Affero General Public License v3.0
+// license that can be found in the LICENSE file
 
-function NamedDivider({ name, id }: { name: string; id: string }) {
-    return (
-        <Box id={id} position="relative" padding="10">
-            <Divider />
-            <AbsoluteCenter bg="black" px="4">
-                {name}
-            </AbsoluteCenter>
-        </Box>
-    );
+// chakra-ui
+import { AbsoluteCenter, Box, Divider, Text } from "@chakra-ui/react";
+import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons';
+
+interface NamedDividerProps {
+  name: string;
+  id: string;
+  isExpandable?: boolean;
+  isExpanded?: boolean;
+  onToggle?: () => void;
+}
+
+function NamedDivider({ name, id, isExpandable = false, isExpanded = false, onToggle }: NamedDividerProps) {
+  return (
+    <Box id={id} position="relative" padding="10" cursor={isExpandable ? "pointer" : "default"} onClick={isExpandable ? onToggle : undefined}>
+      <Divider />
+      <AbsoluteCenter bg="black" px="4">
+        <Text display="flex" alignItems="center">
+          {name}
+          {isExpandable && (
+            isExpanded ? <ChevronUpIcon ml={2} /> : <ChevronDownIcon ml={2} />
+          )}
+        </Text>
+      </AbsoluteCenter>
+    </Box>
+  );
 }
 
 export default NamedDivider;
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,10 +3,10 @@
 // license that can be found in the LICENSE file.
 
 // react
-import { useEffect } from 'react';
+import { useState, useEffect, lazy, Suspense } from 'react';
 
 // chakra-ui
-import { Box } from "@chakra-ui/react";
+import { Box, Spinner } from "@chakra-ui/react";
 
 // custom components
 import NamedDivider from '../components/NamedDivider';
@@ -14,32 +14,61 @@ import NamedDivider from '../components/NamedDivider';
 // sections
 import Preface from '../sections/Preface';
 import Introduction from '../sections/Introduction';
-import Projects from '../sections/Projects';
-import Skills from '../sections/Skills';
-import AboutMe from '../sections/AboutMe';
-import AboutSite from '../sections/AboutSite';
-import Contact from '../sections/Contact';
+
+// Lazy load the other components
+const Projects = lazy(() => import('../sections/Projects'));
+const Skills = lazy(() => import('../sections/Skills'));
+const AboutMe = lazy(() => import('../sections/AboutMe'));
+const AboutSite = lazy(() => import('../sections/AboutSite'));
+const Contact = lazy(() => import('../sections/Contact'));
 
 function HomePage() {
+    const [showContent, setShowContent] = useState(false);
+    const [contentLoaded, setContentLoaded] = useState(false);
+    
     useEffect(() => {
         document.title = 'Kaden Bilyeu | Home';
     }, []);
+
+    const toggleContent = () => {
+        setShowContent(!showContent);
+        if (!contentLoaded) {
+            // Start loading the content
+            setContentLoaded(true);
+        }
+    };
 
     return (
         <Box bg="black" color="white" minHeight="100vh">
             <Preface />
             <NamedDivider name="Introduction" id="introduction" />
             <Introduction />
-            <NamedDivider name="Projects" id="projects" />
-            <Projects />
-            <NamedDivider name="Skills" id="skills" />
-            <Skills />
-            <NamedDivider name="About Me" id="aboutme" />
-            <AboutMe />
-            <NamedDivider name="About The Site" id="aboutsite" />
-            <AboutSite />
-            <NamedDivider name="Contact" id="contact" />
-            <Contact />
+            
+            <NamedDivider 
+                name={showContent ? "Projects" : "Click for More"} 
+                id="projects" 
+                isExpandable={true}
+                isExpanded={showContent}
+                onToggle={toggleContent}
+            />
+
+            {contentLoaded && (
+                <Suspense fallback={<Box textAlign="center" py={4}><Spinner /></Box>}>
+                    {showContent && (
+                        <>
+                            <Projects />
+                            <NamedDivider name="Skills" id="skills" />
+                            <Skills />
+                            <NamedDivider name="About Me" id="aboutme" />
+                            <AboutMe />
+                            <NamedDivider name="About The Site" id="aboutsite" />
+                            <AboutSite />
+                            <NamedDivider name="Contact" id="contact" />
+                            <Contact />
+                        </>
+                    )}
+                </Suspense>
+            )}
         </Box>
     );
 }
