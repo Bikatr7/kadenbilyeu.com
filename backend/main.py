71bb3b299e06785d56861edc6509c1fda57d26bc
Bikatr7 <Bikatr7@proton.me>
2024-07-24 14:12:09 -0600

    added start of backend

---
+++ backend/main.py
@@ -0,0 +1,92 @@
+## Copyright 2024 Kaden Bilyeu (Bikatr7) (https://github.com/Bikatr7) (https://github.com/Bikatr7/kadenbilyeu.com) (https://kadenbilyeu.com)
+## Use of this source code is governed by an GNU Affero General Public License v3.0
+## license that can be found in the LICENSE file.
+
+import os
+from fastapi import FastAPI, HTTPException, status, Depends
+from fastapi.security import  HTTPBasicCredentials, HTTPBasic
+from fastapi.middleware.cors import CORSMiddleware
+from passlib.context import CryptContext
+from pydantic import BaseModel
+import pyotp
+
+##-----------------------------------------start-of-utility-functions----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def get_url() -> str:
+    if(ENVIRONMENT == "development"):
+        return "http://api.localhost:5000"
+    
+    return "https://api.kadenbilyeu.com"
+
+def get_env_variables() -> None:
+
+    """
+
+    Only used in development. This function reads the .env file and sets the environment variables.
+
+    """
+
+    with open(".env") as f:
+        for line in f:
+            key, value = line.strip().split("=")
+            os.environ[key] = value
+
+##-----------------------------------------start-of-pydantic-models----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class TOTPVerify(BaseModel):
+    code:str
+
+##-----------------------------------------start-of-main----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+app = FastAPI()
+
+## CORS setup
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+TURNSTILE_SECRET_KEY = os.environ.get("TURNSTILE_SECRET_KEY")
+ENVIRONMENT = os.environ.get("ENVIRONMENT", "development")
+
+ADMIN_USER = os.environ.get("ADMIN_USER")
+ADMIN_PASS_HASH = os.environ.get("ADMIN_PASS_HASH")
+TOTP_SECRET = os.environ.get("TOTP_SECRET")
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+security = HTTPBasic()
+
+## Turnstile verification endpoint won't be used if the secret key is not set
+## but for the other endpoints, we need to make sure the root keys are set
+if(not any([ADMIN_USER, ADMIN_PASS_HASH])):
+    get_env_variables()
+    ADMIN_USER = os.environ.get("ADMIN_USER")
+    ADMIN_PASS_HASH = os.environ.get("ADMIN_PASS_HASH")
+    TOTP_SECRET = os.environ.get("TOTP_SECRET")
+
+assert ADMIN_USER, "ADMIN_USER environment variable not set"
+assert ADMIN_PASS_HASH, "ADMIN_PASS_HASH environment variable not set"
+assert TOTP_SECRET, "TOTP_SECRET environment variable not set"
+
+def verify_credentials(credentials:HTTPBasicCredentials):
+    if(not(credentials.username == ADMIN_USER and pwd_context.verify(credentials.password, ADMIN_PASS_HASH))):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+
+def get_current_user(credentials:HTTPBasicCredentials = Depends(security)):
+    verify_credentials(credentials)
+    return credentials.username
+
+def verify_totp(data:TOTPVerify, user:str = Depends(get_current_user)):
+    totp = pyotp.TOTP(TOTP_SECRET) # type: ignore
+    if(not totp.verify(data.code)):
+        raise HTTPException(status_code=400, detail="Invalid TOTP code")
+    return {"message": "TOTP code is valid"}
---
+++ backend/setup.py
@@ -0,0 +1,81 @@
+## Copyright 2024 Kaden Bilyeu (Bikatr7) (https://github.com/Bikatr7) (https://github.com/Bikatr7/kadenbilyeu.com) (https://kadenbilyeu.com)
+## Use of this source code is governed by an GNU Affero General Public License v3.0
+## license that can be found in the LICENSE file.
+
+## built-in libraries
+import sys
+import subprocess
+import os
+
+## Define the paths relative to the current working directory of the script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+FRONTEND_ENV = os.path.join(current_dir, "../frontend/.env")
+BACKEND_ENV = os.path.join(current_dir, ".env")
+
+##-------------------start-of-install_dependencies()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+def install_dependencies() -> None:
+   
+    ## Install python dependencies from requirements.
+    try:
+        assert os.path.exists('requirements.txt')
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
+
+        print("Python dependencies installed successfully")
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+    except AssertionError:
+        print("Error: requirements.txt file not found")
+        sys.exit(1)
+
+##-------------------start-of-setup_local_environment()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+def setup_local_environment() -> None:
+
+    env_to_key_local = {
+        "ADMIN_USER": "admin",
+        "ADMIN_PASS_HASH": "$2b$12$wOtWoEqVCCT6lARGKj6aJOSMKKel.R6r5c2ibzGV.k4dKzEjT5m4O",
+        "TOTP_SECRET": "MV1ZGTJ5DI2PBBLJ6YDVTNTMCREFP4EW",
+        "NODE_ENV": "development",
+    }
+
+
+    try:
+
+        if(len(sys.argv) > 1 and sys.argv[1] == 'local'):
+
+            print("Setting up local environment...")
+
+            to_write_frontend = env_to_key_local["NODE_ENV"] + "\n"
+            to_write_backend = "ADMIN_USER=" + env_to_key_local["ADMIN_USER"] + "\n" + "ADMIN_PASS_HASH=" + env_to_key_local["ADMIN_PASS_HASH"] + "\n" + "TOTP_SECRET=" + env_to_key_local["TOTP_SECRET"] + "\n" + "ENVIRONMENT=development" + "\n"
+          
+        else:
+
+            print("Setting up production environment...")
+
+            to_write_frontend = 'NODE_ENV=production\n'
+        
+        os.makedirs(os.path.dirname(FRONTEND_ENV), exist_ok=True)
+
+        with open(FRONTEND_ENV, 'w') as f:
+            f.write(to_write_frontend)
+
+        with open(BACKEND_ENV, 'w') as f:
+            f.write(to_write_backend)
+
+        print("Environment setup successfully")
+
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+##-------------------start-of-main()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def main():
+    install_dependencies()
+    setup_local_environment()
+
+if(__name__ == "__main__"):
+    main()
