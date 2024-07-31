8c3fa428f24c8a99166a1ac4abaf0569ab7d46ab
Bikatr7 <Bikatr7@proton.me>
2024-07-31 02:53:57 -0600

    [bug fix] allowed BlogPostUpdate model for optional types

--- backend/schemas.py
+++ backend/schemas.py
@@ -2,6 +2,10 @@
 ## Use of this source code is governed by an GNU Affero General Public License v3.0
 ## license that can be found in the LICENSE file.
 
+## built-in imports
+import typing
+
+## third-party imports
 from pydantic import BaseModel
 from datetime import datetime
 from uuid import UUID
@@ -14,8 +18,10 @@ class BlogPostBase(BaseModel):
 class BlogPostCreate(BlogPostBase):
     pass
 
-class BlogPostUpdate(BlogPostBase):
-    pass
+class BlogPostUpdate(BaseModel):
+    title:typing.Optional[str] = None
+    content:typing.Optional[str] = None
+    author:typing.Optional[str] = None
 
 class BlogPost(BlogPostBase):
     id:UUID
